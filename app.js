aa78a1535e2aa313ef74c43b841504684406415f
josueace <josueacev2@gmail.com>
2020-05-12 14:50:31 -0400

    passport

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+
+const flash = require("connect-flash");
+const session = require("express-session");
+
 const path = require('path');
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User.model.js");
+
+
 
 mongoose
   .connect('mongodb://localhost/auth-with-passport', {
@@ -18,10 +28,57 @@ mongoose
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
 
 const app = express();
+
+// app.js
+app.use(session({
+  secret: "authentication-with-passport",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req,username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+// app.js
+app.use(session({
+  secret: "authentication-with-passport",
+  resave: true,
+  saveUninitialized: true
+}));
+
+// app.js
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Middleware Setup
 app.use(logger('dev'));
